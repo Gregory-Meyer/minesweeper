ab8f4887f7054fe43a911aab49b656e9dc90be8d
Gregory Meyer <gregjm@umich.edu>
2018-12-19 18:25:03 -0500

    initial commit

---
+++ include/sweep/util/state.h
@@ -0,0 +1,148 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2018, Gregory Meyer
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in
+//   the documentation and/or other materials provided with the
+//   distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived
+//   from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef SWEEP_UTIL_STATE_H
+#define SWEEP_UTIL_STATE_H
+
+#include <sweep/util/types.h>
+
+#include <cassert>
+#include <functional>
+#include <iosfwd>
+#include <optional>
+#include <utility>
+#include <variant>
+
+namespace sweep::util {
+namespace state {
+
+class Unopened { };
+
+class Opened {
+public:
+	constexpr explicit Opened(i8 x) : value_{ x } {
+		assert(x >= 0 && x <= 8);
+	}
+
+	constexpr operator i8() const noexcept {
+		return get();
+	}
+
+	constexpr i8 get() const noexcept {
+		return value_;
+	}
+
+private:
+	i8 value_;
+};
+
+class Mine { };
+
+template <typename C, typename ...Ts>
+struct IsInvocableUnionType : std::conjunction<std::is_invocable<C, Ts>...> { };
+
+template <typename C, typename ...Ts>
+inline constexpr bool IS_INVOCABLE_UNION = IsInvocableUnionType<C, Ts...>::value;
+
+template <typename C, typename ...Ts>
+struct IsNothrowInvocableUnionType : std::conjunction<std::is_nothrow_invocable<C, Ts>...> { };
+
+template <typename C, typename ...Ts>
+inline constexpr bool IS_NOTHROW_INVOCABLE_UNION = IsNothrowInvocableUnionType<C, Ts...>::value;
+
+template <typename C, typename ...Ts>
+struct CommonInvokeResultType : std::common_type<std::invoke_result_t<C, Ts>...> { };
+
+template <typename C, typename ...Ts>
+using CommonInvokeResult = typename CommonInvokeResultType<C, Ts...>::type;
+
+template<class... Ts> struct Overload : Ts... { using Ts::operator()...; };
+
+template<class... Ts> Overload(Ts...) -> Overload<Ts...>;
+
+class State {
+public:
+	constexpr explicit State(Unopened) noexcept : data_{ Unopened{ } } { }
+
+	constexpr explicit State(Opened opened) noexcept : data_{ opened } { }
+
+	constexpr explicit State(Mine) noexcept : data_{ Mine{ } } { }
+
+	template <
+		typename C, std::enable_if_t<IS_INVOCABLE_UNION<C, Unopened, Opened, Mine>, int> = 0
+	>
+	constexpr CommonInvokeResult<C, Unopened, Opened, Mine> visit(C &&callable) const
+	noexcept(IS_NOTHROW_INVOCABLE_UNION<C, Unopened, Opened, Mine>) {
+		return std::visit(std::forward<C>(callable), data_);
+	}
+
+	constexpr bool is_unopened() const noexcept {
+		return std::holds_alternative<Unopened>(data_);
+	}
+
+	constexpr bool is_opened() const noexcept {
+		return std::holds_alternative<Opened>(data_);
+	}
+
+	constexpr bool is_mine() const noexcept {
+		return std::holds_alternative<Mine>(data_);
+	}
+
+	constexpr std::optional<Opened> as_opened() const noexcept {
+		return visit(Overload{
+			[](Opened o) -> std::optional<Opened> { return { o }; },
+			[](auto) -> std::optional<Opened> { return std::nullopt; }
+		});
+	}
+
+private:
+	std::variant<Unopened, Opened, Mine> data_;
+};
+
+std::ostream& operator<<(std::ostream &os, Unopened);
+
+std::ostream& operator<<(std::ostream &os, Opened o);
+
+std::ostream& operator<<(std::ostream &os, Mine);
+
+std::ostream& operator<<(std::ostream &os, State state);
+
+} // namespace state
+
+using state::Unopened; using state::Opened; using state::Mine; using state::State;
+using state::operator<<;
+
+} // namespace sweep::util
+
+#endif
---
+++ include/sweep/util/types.h
@@ -0,0 +1,115 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2018, Gregory Meyer
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in
+//   the documentation and/or other materials provided with the
+//   distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived
+//   from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef SWEEP_UTIL_TYPES_H
+#define SWEEP_UTIL_TYPES_H
+
+#include <cstdint>
+
+#include <gsl/gsl_util>
+
+namespace sweep::util {
+inline namespace types {
+
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+
+using i8 = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+
+using usize = std::size_t;
+using isize = std::ptrdiff_t;
+
+using f32 = float;
+using f64 = double;
+
+inline namespace literals {
+
+constexpr u8 operator""_u8(unsigned long long x) noexcept {
+	return gsl::narrow_cast<u8>(x);
+}
+
+constexpr u16 operator""_u16(unsigned long long x) noexcept {
+	return gsl::narrow_cast<u16>(x);
+}
+
+constexpr u32 operator""_u32(unsigned long long x) noexcept {
+	return gsl::narrow_cast<u32>(x);
+}
+
+constexpr u64 operator""_u64(unsigned long long x) noexcept {
+	return gsl::narrow_cast<u64>(x);
+}
+
+constexpr i8 operator""_i8(unsigned long long x) noexcept {
+	return gsl::narrow_cast<i8>(x);
+}
+
+constexpr i16 operator""_i16(unsigned long long x) noexcept {
+	return gsl::narrow_cast<i16>(x);
+}
+
+constexpr i32 operator""_i32(unsigned long long x) noexcept {
+	return gsl::narrow_cast<i32>(x);
+}
+
+constexpr i64 operator""_i64(unsigned long long x) noexcept {
+	return gsl::narrow_cast<i64>(x);
+}
+
+constexpr usize operator""_usize(unsigned long long x) noexcept {
+	return gsl::narrow_cast<usize>(x);
+}
+
+constexpr isize operator""_isize(unsigned long long x) noexcept {
+	return gsl::narrow_cast<isize>(x);
+}
+
+constexpr f32 operator""_f32(long double x) noexcept {
+	return gsl::narrow_cast<f32>(x);
+}
+
+constexpr f64 operator""_f64(long double x) noexcept {
+	return gsl::narrow_cast<f64>(x);
+}
+
+} // inline namespace literals
+} // inline namespace types
+} // namespace sweep::util
+
+#endif
